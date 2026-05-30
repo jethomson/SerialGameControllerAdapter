3896a6971801dfa55f044291b5e7ef330a08cfab
jethomson <jonathan_e_thomson@yahoo.com>
2026-05-30 16:25:33 -0400

    Minor tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@
 
 enum ControllerType : uint8_t {
     CTL_NC = 255,  // no input device, always outputs 0x00 so code operates properly when a controller is not connected.
-    GP_GPIO = 0,    // custom input device using one GPIO per button
+    GP_GPIO = 0,   // custom input device using one GPIO per button
     GP_NES = 1,    // wired NES controller
     GP_SNES = 2,   // wired SNES controller
     GP_PSX = 3,    // wired playstation controller using playstation connector
